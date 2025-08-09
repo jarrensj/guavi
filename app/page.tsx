ec42183b0b8e2d2455984f1f62f52e6435dd4b8c
Jarren San Jose <sanjose.jarren@gmail.com>
2025-08-08 23:00:16 -0700

    set photo as background photo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen p-8 flex flex-col items-center justify-center text-center">
-      <h1 className="text-2xl font-bold mb-4">Guavi</h1>
+      <h1 className="text-2xl font-bold mb-4 text-white drop-shadow-lg">Guavi</h1>
     </main>
   );
 }
